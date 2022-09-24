af6e3c1628cab91b25060008bcf4f70d9b0f1e23
soychun <110978406+soychun@users.noreply.github.com>
2022-09-25 02:15:53 +0900

    22_09_25

--- BaekJoon_study/sw역량테스트/청소년상어_2.py
+++ BaekJoon_study/sw역량테스트/청소년상어_2.py
@@ -1,3 +1,6 @@
+# 물고기 이동에 굉장히 많은 영감을 준 블로그
+# https://developer-ellen.tistory.com/68
+
 dx = [-1,-1,0,1,1,1,0,-1]
 dy = [0,-1,-1,-1,0,1,1,1]
 g = []
@@ -12,20 +15,33 @@ print(g)
 g[0][0][0] = -1 #상어가 물고기를 먹음
 print(g)
 def fish_move():
-    for i in range(16+1):
+    for i in range(1,16+1):
+        f_x,f_y = -1,-1
         for x in range(4):
             for y in range(4):
                 if g[x][y][0]==i:
-                    print(i)
-                    b = g[x][y][1]
-                    for b_ in range(8):
+                    f_x,f_y = x,y
+                    print(x,y)
+                    break         #안바꾸고 먼저 찾기
+        print(i)
+        b = g[f_x][f_y][1]
+                    # print(i)
+                    # b = g[x][y][1]
+        for b_ in range(8):
+            nx = f_x + dx[b]
+            ny = f_y + dy[b]
+            if 0<=nx<4 and 0<=ny<4 and (g[nx][ny][0]!=-1):
+                print('--')
+                g[f_x][f_y],g[nx][ny] = g[nx][ny],g[f_x][f_y]
+                break
 
-                        nx = x + dx[b]
-                        ny = y + dy[b]
-                        if 0<=nx<4 and 0<=ny<4 and (g[nx][ny][0]!=-1):
-                            g[x][y],g[nx][ny] = g[nx][ny],g[x][y]
-                        else:
-                            b+=1
+            else:
+                b=(b+1)%8
+
 
 
 fish_move()
+print(g)
+
+def eat():
+    
