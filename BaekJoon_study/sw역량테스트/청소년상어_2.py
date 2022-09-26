6eeba088acfc87c3755a7faf04db0da6b0b1bfdb
soychun <110978406+soychun@users.noreply.github.com>
2022-09-26 12:01:16 +0900

    22_09_26

--- BaekJoon_study/sw역량테스트/청소년상어_2.py
+++ BaekJoon_study/sw역량테스트/청소년상어_2.py
@@ -1,11 +1,14 @@
 # 물고기 이동에 굉장히 많은 영감을 준 블로그
 # https://developer-ellen.tistory.com/68
+그런데 또 하다말음
+
 
 dx = [-1,-1,0,1,1,1,0,-1]
 dy = [0,-1,-1,-1,0,1,1,1]
 g = []
 
 cnt_max = 0
+cnt_list = []
 
 for i in range(4):
     tmp = list(map(int,input().split()))
@@ -14,7 +17,9 @@ print(g)
 
 g[0][0][0] = -1 #상어가 물고기를 먹음
 print(g)
-def fish_move():
+s_x,s_y = 0,0
+def dfs(s_x,s_y):
+    global cnt_max
     for i in range(1,16+1):
         f_x,f_y = -1,-1
         for x in range(4):
@@ -37,11 +42,37 @@ def fish_move():
 
             else:
                 b=(b+1)%8
+    d = g[s_x][s_y][1]
+    p_f_eat = []
+    for i in range(1, 5):
+        nx = s_x + dx[d] * i
+        ny = s_y + dy[d] * i
+        if 0 <= nx < 4 and 0 <= ny < 4 and g[nx][ny][0] > 0:
+            p_f_eat.append([nx, ny])
+    if len(p_f_eat) == 0:
+        print(cnt_list)
+        return
+    else:
+        for i in range(len(p_f_eat)):
+            f_x, f_y = p_f_eat[i][0], p_f_eat[i][1]
+            cnt_max += g[f_x][f_y][0]
+            cnt_list.append(g[f_x][f_y][0])
+
+            g[f_x][f_y][0] = -1
+
+            dfs(f_x,f_y)
+            cnt_max -= g[f_x][f_y][0]
+            cnt_list.pop()
 
 
 
-fish_move()
+dfs(0,0)
 print(g)
+print(cnt_max)
 
-def eat():
-    
+
+
+
+
+
+
---
+++ This_is_coding_test/DFS_BFS/복습_22_09_26.py
@@ -0,0 +1,40 @@
+# 조합 기본 (for문 사용)
+
+n = 4
+r = 2
+s = []
+def dfs(L,B):
+    if L == r:
+        print(s)
+    else:
+        for i in range(B,n):
+            s.append(i+1)
+            dfs(L+1,i+1)
+            s.pop()
+dfs(0,0)
+
+# 삼성DFS를 이용한 조합 구현
+# 조합+DFS 알고리즘은 00 중 M개를 골랐을 때 최소(최대)가 되는 값을 찾으세요 (순서가 없는 경우)
+# 와 같은 형식의 문제가 있을 때 사용 가능하다
+# 보통 재귀함수를 이용
+
+
+# 조합 응용 (for문 안사용)  : 내일도 복습할 것
+# https://kimjingo.tistory.com/205
+from collections import deque
+s = deque()
+n = 4
+r = 3
+def do_somthing(s):
+    print(s)
+def dfs_notfor(s,L,D):
+    if len(s)==r:
+        do_somthing(s)
+        return
+    elif D==n:
+        return
+    s.append(D+1)
+    dfs_notfor(s,L+1,D+1)
+    s.pop()
+    dfs_notfor(s,L+1,D+1)
+dfs_notfor(s,0,0)
