aa33dfbf514eb0324b3fd3144d91ca64477fc6bd
soychun <110978406+soychun@users.noreply.github.com>
2022-09-29 19:27:50 +0900

    22_09_29

--- BaekJoon_study/sw역량테스트/ABC_배열 돌리기.py
+++ BaekJoon_study/sw역량테스트/ABC_배열 돌리기.py
@@ -1,3 +1,7 @@
+zip은 서로 다른 배열을 하나씩 묶어주는 메소드이다
+zip이 아니라, 사실 zip(*)을 이용한 unzip을 다시 리스트로 묶어줌으로써 해체 후 재합성
+그 다음에 reverse를 [::-1]
+
 def print_g(g):
     for i in range(len(g)):
         print(g[i])
---
+++ BaekJoon_study/sw역량테스트/ABC_배열중력.py
@@ -0,0 +1,1 @@
+array_original = [[1,2,]]
