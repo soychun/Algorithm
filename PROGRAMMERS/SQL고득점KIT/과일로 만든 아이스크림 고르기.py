0f93dc3646dadf8cfe3b708e7468404ae9d4c8de
soychun <110978406+soychun@users.noreply.github.com>
2022-10-30 02:53:25 +0900

    22_10_30

---
+++ PROGRAMMERS/SQL고득점KIT/과일로 만든 아이스크림 고르기.py
@@ -0,0 +1,10 @@
+LIKE 문법
+https://lcs1245.tistory.com/entry/SQL-LIKE-%EC%97%B0%EC%82%B0%EC%9E%90-%EB%AC%B8%EC%9E%90%EC%97%B4-%EB%B6%80%EB%B6%84%EC%9D%BC%EC%B9%98-%EA%B2%80%EC%83%89
+
+SELECT *
+FROM 테이블
+WHERE 칼럼 LIKE '쓰고싶은 문장'
+
+
+JOIN 문법
+https://yoo-hyeok.tistory.com/98
