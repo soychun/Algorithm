2d01c63797a7a3e7eb38e96f33cd0ba61bf01b73
soychun <110978406+soychun@users.noreply.github.com>
2023-02-03 01:00:57 +0900

    23_02_03

--- 기초/Combination.py
+++ 기초/Combination.py
@@ -1,21 +1,10 @@
 n,m = map(int,input().split())
 
-n = [i for i in range(1,n+1)]
-
-def dfs(L,B):
-    if m==len(s):
-        print(s)
-    else:
+n = [i for i in
         for i in range(B,len(n)):
             # print(i)
             if v[i]:
-                continue
-            else:
-                s.append(n[i])
-                dfs(L+1,i+1)
-                s.pop()
-
-
+                c
 s = []
 v = [False]*len(n)
 dfs(0,0)
